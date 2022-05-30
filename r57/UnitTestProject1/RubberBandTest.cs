3ab64a5734476dbe1d45022c785a911ee2e32dc2
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-05-30 20:27:11 +0900

    re

---
+++ r57/UnitTestProject1/RubberBandTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RubberBandTest
+    {
+        PictureBox pb;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pb).BeginInit();
+            pb.Location = new System.Drawing.Point(260, 26);
+            pb.Name = "PictureBox1";
+            pb.Size = new System.Drawing.Size(465, 332);
+            pb.TabIndex = 29;
+            pb.TabStop = false;
+            ((System.ComponentModel.ISupportInitialize)pb).EndInit();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var sut = new ClassLibrary1.RubberBand(pb);
+            var image = new System.Drawing.Bitmap(100,200);
+            sut.setImage(image);
+            Assert.AreEqual(pb.Size, sut.Size);
+            Assert.AreEqual(new System.Drawing.Point(0,0), sut.Location);
+            Assert.AreEqual(System.Drawing.Color.FromArgb(255, 0, 0), sut.Color);
+        }
+    }
+}
