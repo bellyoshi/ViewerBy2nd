a084f8d77c328bafaf2a52b2ac17c18b1c5a6207
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-06-01 15:17:10 +0900

    refact

--- r57/pdfsecondmonitor-r57/SetWinWidthModule.vb
+++ r57/pdfsecondmonitor-r57/SetWinWidthModule.vb
@@ -1,20 +1,19 @@
 ﻿'ウインドウ幅に合わせる機能の提供
 Public Class SetWinWidthModule
 
-    Private _pictureBox As PictureBox
-    Private _thumbnailPictureBox As PictureBox
-    Private _vScrollBar1 As VScrollBar
+    Private pbDisp As PictureBox
+    Private pbThumbnail As PictureBox
+    Private scrollBar As VScrollBar
 
     Public Sub New(disp As PictureBox, thumb As PictureBox, scroll As VScrollBar)
-        _pictureBox = disp
-
-        _thumbnailPictureBox = thumb
-        _vScrollBar1 = scroll
+        pbDisp = disp
+        pbThumbnail = thumb
+        scrollBar = scroll
     End Sub
 
     Private ReadOnly Property Image As Bitmap
         Get
-            Return _thumbnailPictureBox.Image
+            Return pbThumbnail.Image
         End Get
     End Property
 
@@ -34,23 +33,23 @@ Public Class SetWinWidthModule
         If Image Is Nothing Then
             Exit Sub
         End If
-        If Not CanSetWindowWidthRate(Image.Size, _pictureBox.Size) Then
+        If Not CanSetWindowWidthRate(Image.Size, pbDisp.Size) Then
             Exit Sub
         End If
 
         Dim ImageY As Integer
-        If _vScrollBar1.Value + GetSetWinImageHeight() > Image.Height Then
+        If scrollBar.Value + GetSetWinImageHeight() > Image.Height Then
             ImageY = CType(Image.Height - GetSetWinImageHeight(), Integer)
         Else
-            ImageY = _vScrollBar1.Value
+            ImageY = scrollBar.Value
         End If
         Dim rect = New Rectangle(0, ImageY, Image.Width, GetSetWinImageHeight())
         Dim bmpNew As Bitmap = Image.Clone(rect, Image.PixelFormat)
-        _pictureBox.Image = bmpNew
-        _pictureBox.SizeMode = PictureBoxSizeMode.Zoom
+        pbDisp.Image = bmpNew
+        pbDisp.SizeMode = PictureBoxSizeMode.Zoom
     End Sub
     Private Function GetSetWinImageHeight() As Integer
-        Dim pbSize = _pictureBox.Size
+        Dim pbSize = pbDisp.Size
         Return CType(pbSize.Height / pbSize.Width * Image.Width, Integer)
     End Function
 
