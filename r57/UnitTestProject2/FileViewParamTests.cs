d3ed24a0f6de1d648b49dedb0af42b2620ef3f0f
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-06-06 08:14:52 +0900

    FileViewParam移植

---
+++ r57/UnitTestProject2/FileViewParamTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViewerBy2ndLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewerBy2ndLib.Tests
+{
+    [TestClass()]
+    public class FileViewParamTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        { 
+             var sut = new FileViewParam(@"c:\test.txt");
+            Assert.AreEqual("test.txt",sut.ToString());
+        }
+    }
+}
---
+++ r57/ViewerBy2ndLib/FileViewParam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewerBy2ndLib
+{
+    public class FileViewParam
+    {
+        public string FileName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{System.IO.Path.GetFileName(FileName)}";
+        }
+
+        public FileViewParam(string filename)
+        {
+            this.FileName = filename;
+        }
+        public FileViewParam()
+        {
+            //シリアル化用
+        }
+    }
+}
