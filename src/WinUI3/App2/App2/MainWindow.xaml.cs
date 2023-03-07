0c2c6e829713295f18e48574b2c69c634d4437f1
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-03-07 14:28:14 +0900

    app2

---
+++ src/WinUI3/App2/App2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using AppUIBasics;
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App2
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            //var newWindow = WindowHelper.CreateWindow();
+            //var rootPage = new BlankPage1();
+            //newWindow.Content = rootPage;
+            //newWindow.Activate();
+
+            //BlankWindow1‚ðŠJ‚­
+            var newWindow = new BlankWindow1();
+            newWindow.Activate();
+        }
+    }
+}
+            
