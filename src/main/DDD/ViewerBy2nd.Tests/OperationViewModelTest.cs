2e4655ff8ecd43cb9d94487b5342f2f6423e288a
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-07-09 21:47:29 +0900

    test

--- src/main/DDD/ViewerBy2nd.Tests/OperationViewModelTest.cs
+++ src/main/DDD/ViewerBy2nd.Tests/OperationViewModelTest.cs
@@ -5,14 +5,37 @@ namespace ViewerBy2nd.Tests
     [TestClass]
     public class OperationViewModelTest
     {
+        private OperationViewModel _viewModel;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _viewModel = new OperationViewModel();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
             OperationViewModel model = new();
+            string file = "testfile.pdf";
+            model.AddFile(file);
+            Assert.AreEqual(1, model.FileList.Count);
+            Assert.AreEqual("testfile.pdf", model.FileList[0].FileName);
+
 
         }
-    }
+   
+        [TestMethod]
+        public void AddFile_RaisesFileListChangedEvent()
+        {
+            var eventRaised = false;
+            _viewModel.FileListChanged += () => eventRaised = true;
 
+            _viewModel.AddFile("testfile");
+
+            Assert.IsTrue(eventRaised, "FileListChanged event was not raised after adding a file");
+        }
+    }
 
 }
       
--- src/main/DDD/ViewerBy2nd.WinFormsControlLibrary/OperationViewModel.cs
+++ src/main/DDD/ViewerBy2nd.WinFormsControlLibrary/OperationViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ViewerBy2ndLib;
 
-namespace ViewerBy2nd.WinFormsControlLibrary
+public class OperationViewModel
 {
-    public class OperationViewModel
+    private Size bound;
+
+    // FileList property is made private set to control its modification through the AddFile method
+    public IList<FileViewParam> FileList { get; set; } = new List<FileViewParam>();
+
+    // Event that is fired whenever a file is added
+    public event Action? FileListChanged;
+
+    public void AddFile(string file)
     {
+        FileList.Add(new FileViewParam(file, bound));
+
+        // Invoke the FileListChanged event whenever a file is added
+        FileListChanged?.Invoke();
     }
+
+
 }
