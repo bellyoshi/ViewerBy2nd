edded7d7b9b7b6868b8507fbb2cd20da1c44777b
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-10-08 16:24:52 +0900

    content

---
+++ src/prototype/DuoPDFViewer/BlankWindow1.xaml.cs
@@ -0,0 +1,102 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace DuoPDFViewer
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankWindow1 : Window
+    {
+        public BlankWindow1()
+        {
+            this.InitializeComponent();
+
+            var appWindow = GetAppWindow();
+
+
+
+            //セカンドモニターに移動する
+            var d = DisplayArea.FindAll();
+            
+            //var a = d.FirstOrDefault(c => !c.IsPrimary);todo:実行時エラー
+            DisplayArea displayArea = d[0];
+            for(int i=0; i < d.Count; i++)
+            {
+                if (!d[i].IsPrimary)
+                {
+                    displayArea = d[i];
+                    break;
+                }
+            }
+
+
+
+            RectInt32 rect = new RectInt32()
+            {
+                X = 0,
+                Y = 0,
+                Width = displayArea.WorkArea.Width,
+                Height = displayArea.WorkArea.Height
+            };
+            appWindow.MoveAndResize(rect, displayArea);
+            
+
+            //最前面に表示する
+            var olp = OverlappedPresenter.Create();
+            olp.IsMaximizable = false;
+            //最大化ボタン
+            olp.IsMinimizable = false; //最小化ボタン
+            olp.IsResizable = false; //サイズ変更
+            olp.IsAlwaysOnTop = false; //常に最前面に表示
+            olp.IsModal= false; //モーダル・モードレス
+            appWindow.SetPresenter(olp);
+            //フルスクリーンプレゼンターを挿入する
+            var fp = FullScreenPresenter.Create();
+            appWindow.SetPresenter(fp);
+
+        }
+
+
+
+
+        
+
+        public AppWindow GetAppWindow()
+        {
+            
+                // WinUI3のウインドウのハンドルを取得
+                var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+                // そのウインドウのIDを取得
+                Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+                // そこからAppWindowを取得する
+                Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                return appWindow;
+            
+        }
+    }
+
+}
+
--- src/prototype/DuoPDFViewer/MainWindow.xaml.cs
+++ src/prototype/DuoPDFViewer/MainWindow.xaml.cs
@@ -1,17 +1,16 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Media.Imaging;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Data.Pdf;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +22,96 @@ namespace DuoPDFViewer
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        BlankWindow1 newWindow;
         public MainWindow()
         {
             this.InitializeComponent();
+            Handle = this;
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void SecondButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            //var newWindow = WindowHelper.CreateWindow();
+            //var rootPage = new BlankPage1();
+            //newWindow.Content = rootPage;
+            //newWindow.Activate();
+
+            //BlankWindow1を開く
+            newWindow = new BlankWindow1();
+            newWindow.Activate();
         }
+        private void Window_Closed(object sender, WindowEventArgs e)
+        {
+            newWindow?.Close();
+        }
+
+
+
+        private PdfDocument doc;
+        private PdfPage page;
+        private uint pageIndex;
+
+        public static MainWindow Handle { get; private set; }
+
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFile file = await GetFile();
+            if (file == null) return;
+
+            doc = await PdfDocument.LoadFromFileAsync(file);
+            pageIndex = 0;
+
+            await DispPage();
+        }
+
+        private async Task DispPage()
+        {
+            page = doc.GetPage(pageIndex);
+            // ビットマップイメージの作成
+            var stream = new Windows.Storage.Streams.InMemoryRandomAccessStream();
+            await page.RenderToStreamAsync(stream);
+            BitmapImage src = new();
+
+            // Imageオブジェクトにsrcをセット
+            imgPdf.Source = src;
+
+            // srcに作成したビットマップイメージを流し込む
+            await src.SetSourceAsync(stream);
+        }
+
+        private static async Task<StorageFile> GetFile()
+        {
+            var openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.List,
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+            };
+            openPicker.FileTypeFilter.Add(".pdf");
+            InitializeWithWindow.Initialize(openPicker,
+             WindowNative.GetWindowHandle(MainWindow.Handle));
+            IAsyncOperation<StorageFile> asyncOp = openPicker.PickSingleFileAsync();
+            StorageFile file = await asyncOp;
+            return file;
+        }
+
+        private async void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (doc.PageCount <= pageIndex + 1)
+                return;
+
+            pageIndex++;
+            await DispPage();
+
+        }
+
+        private async void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (pageIndex <= 0) return;
+            pageIndex--;
+            await DispPage();
+        }
+
     }
 }
+
+            
