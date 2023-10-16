d81e91a8dcce1dfd685ee554007692a5b4b65758
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-10-17 08:43:13 +0900

    #

--- src/prototype/DuoPDFViewer/MainWindow.xaml.cs
+++ src/prototype/DuoPDFViewer/MainWindow.xaml.cs
@@ -4,7 +4,9 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media.Imaging;
 using System;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Windows.Data.Pdf;
 using Windows.Foundation;
 using Windows.Storage;
@@ -22,92 +24,20 @@ namespace DuoPDFViewer
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        private MainViewModel ViewModel { get; } = new();
-        ViewerWindow newWindow;
+        public static MainWindow Handle { get; private set; }
+
+        public static ViewerWindow NewWindow { get; set; }
+
         public MainWindow()
         {
             this.InitializeComponent();
-
             Handle = this;
-        }
 
-        private void SecondButton_Click(object sender, RoutedEventArgs e)
-        {
-            newWindow = new ViewerWindow();
-            newWindow.Activate();
         }
         private void Window_Closed(object sender, WindowEventArgs e)
         {
-            newWindow?.Close();
+            NewWindow?.Close();
         }
-
-
-
-        private PdfDocument doc;
-        private PdfPage page;
-        private uint pageIndex;
-
-        public static MainWindow Handle { get; private set; }
-
-        private async void OpenButton_Click(object sender, RoutedEventArgs e)
-        {
-            StorageFile file = await GetFile();
-            if (file == null) return;
-
-            doc = await PdfDocument.LoadFromFileAsync(file);
-            pageIndex = 0;
-
-            await DispPage();
-        }
-
-        private async Task DispPage()
-        {
-            page = doc.GetPage(pageIndex);
-            // ビットマップイメージの作成
-            var stream = new Windows.Storage.Streams.InMemoryRandomAccessStream();
-            await page.RenderToStreamAsync(stream);
-            BitmapImage src = new();
-
-            // Imageオブジェクトにsrcをセット
-            //imgPdf.Source = src;
-            Model.GetInstance().SetSource(src);
-
-            // srcに作成したビットマップイメージを流し込む
-            await src.SetSourceAsync(stream);
-        }
-
-        private static async Task<StorageFile> GetFile()
-        {
-            var openPicker = new FileOpenPicker
-            {
-                ViewMode = PickerViewMode.List,
-                SuggestedStartLocation = PickerLocationId.DocumentsLibrary
-            };
-            openPicker.FileTypeFilter.Add(".pdf");
-            InitializeWithWindow.Initialize(openPicker,
-             WindowNative.GetWindowHandle(MainWindow.Handle));
-            IAsyncOperation<StorageFile> asyncOp = openPicker.PickSingleFileAsync();
-            StorageFile file = await asyncOp;
-            return file;
-        }
-
-        private async void NextButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (doc.PageCount <= pageIndex + 1)
-                return;
-
-            pageIndex++;
-            await DispPage();
-
-        }
-
-        private async void PreviousButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (pageIndex <= 0) return;
-            pageIndex--;
-            await DispPage();
-        }
-
     }
 }
 
